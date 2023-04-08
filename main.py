cc2c44e6e77d8e940e3b235a85f90d88eda79e8a
CipherLXRD <130173810+CipherLXRD@users.noreply.github.com>
2023-04-08 12:15:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-url = "https://www.example.com"#the website you want to use such as 'https://en.wikipedia.org/wiki/Python_(programming_language)'
+url = "https://en.wikipedia.org/wiki/C++"
 response = requests.get(url)
-data = soup.select('div.some-class span.some-data')#the classes from which you want to extract your data 'mw-parser-output'
+soup = BeautifulSoup(response.content, 'html.parser')
+table = soup.find('table', {'class': 'infobox vevent'})
+rows = table.find_all('tr')
+data = []
 
-with open('data.csv','w',newline = '') as file:
+for row in rows:
+  cols = row.find_all(['th', 'td'])
+  cols = [col.text.strip() for col in cols]
+  data.append([col for col in cols if col])
+
+with open('data.csv', 'w', newline='', encoding='utf-8') as file:
   writer = csv.writer(file)
-  writer.writerow(['Data'])
+  writer.writerow(['Attribute', 'Value'])
   for item in data:
-    writer.writerow([item.text])
+    writer.writerow(item)
