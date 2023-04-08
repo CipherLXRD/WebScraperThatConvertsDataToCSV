e50b0f7c6e494d12ea4a2966884c673f843e135b
CipherLXRD <130173810+CipherLXRD@users.noreply.github.com>
2023-04-08 11:57:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-url = "https://www.example.com"
+url = "https://www.example.com"#the website you want to use such as 'https://en.wikipedia.org/wiki/Python_(programming_language)'
 response = requests.get(url)
-data = soup.select('div.some-class span.some-data')
+data = soup.select('div.some-class span.some-data')#the classes from which you want to extract your data 'mw-parser-output'
 
 with open('data.csv','w',newline = '') as file:
   writer = csv.writer(file)
